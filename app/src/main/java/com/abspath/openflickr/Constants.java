b20762f4ab212ca860a04d81518597c29e62baf6
devzld <779724606@qq.com>
2017-04-15 14:02:00 +0800

    modify

---
+++ app/src/main/java/com/abspath/openflickr/Constants.java
@@ -0,0 +1,11 @@
+package com.abspath.openflickr;
+
+/**
+ * Created by lingdong on 2017/4/15.
+ */
+
+public class Constants {
+
+    public static final String API_KEY = "dee566973f4f639647dcb339ebe37a9e";
+    public static final String api_secret = "0fc58f6bde2b6811";
+}
